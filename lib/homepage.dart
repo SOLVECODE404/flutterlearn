baddd476322b41caa77e026fef7957c463856383
SOLVECODE404 <kutsavsingh@gmail.com>
2023-06-29 21:55:22 +0530

    second

---
+++ lib/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Catalog app'),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('hy'),
+        ),
+      ),
+    );
+  }
+}
