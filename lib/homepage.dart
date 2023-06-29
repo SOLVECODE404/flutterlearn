c426295467727fa71b5bb021341c56929c0730b4
SOLVECODE404 <kutsavsingh@gmail.com>
2023-06-29 21:56:27 +0530

    second

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,21 +3,17 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Material(
-          child: Scaffold(
-            appBar: AppBar(
-              title: Center(
-                child: Text('Catalog app'),
-              ),
-            ),
-            body: Center(
-              child: Container(
-                child: Text('hy'),
-              ),
-            ),
-          ),
-        ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Catalog app'),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('hy'),
+        ),
+      ),
+    );
   }
 }
