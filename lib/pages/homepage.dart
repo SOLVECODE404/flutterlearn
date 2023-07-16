de88e6edb6ea485e8fa71c69f5fe31a34a7d5573
SOLVECODE404 <kutsavsingh@gmail.com>
2023-07-16 23:20:55 +0530

    day  10 completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,13 +1,34 @@
+import 'dart:convert';
+
 import 'package:catalog/pages/catalog.dart';
 import 'package:catalog/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../widgets/item_widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData=jsonDecode(catalogJson);
+    var productsData=decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummylist=List.generate(6, (index) =>catalogModel.items[0]);  
+    final dummylist = List.generate(6, (index) => catalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -7,59 +7,63 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: Container(
-      color: Colors.blue[500],
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            padding: EdgeInsets.zero,
-            child: UserAccountsDrawerHeader(
-              margin: EdgeInsets.zero,
-              accountName: Text("Utsav singh"),
-              accountEmail: Text("utsav@gmail.com"),
-              // currentAccountPicture: CircleAvatar(
-              //   backgroundImage: NetworkImage(
-              //     imageUrl,
-              //   ),
-              // ),
+      child: Container(
+        color: Colors.blue[500],
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                  margin: EdgeInsets.zero,
+                  accountName: Text("Utsav singh"),
+                  accountEmail: Text("utsav@gmail.com"),
+                  // currentAccountPicture: CircleAvatar(
+                  //   child: Image.asset("assets/images/iPhone1.jpg"),
+                  // )
+                  // currentAccountPicture: CircleAvatar(
+                  //   backgroundImage: NetworkImage(
+                  //     imageUrl,
+                  //   ),
+                  // ),
+                  ),
             ),
-          ),
-          ListTile(
-            leading: Icon(
-              CupertinoIcons.home,
-              color: Colors.white,
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.5,
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-            title: Text(
-              "Home",
-              textScaleFactor: 1.5,
-              style: TextStyle(color: Colors.white),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.5,
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-          ),
-          ListTile(
-            leading: Icon(
-              CupertinoIcons.profile_circled,
-              color: Colors.white,
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Mail Me",
+                textScaleFactor: 1.5,
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-            title: Text(
-              "Profile",
-              textScaleFactor: 1.5,
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          ListTile(
-            leading: Icon(
-              CupertinoIcons.mail,
-              color: Colors.white,
-            ),
-            title: Text(
-              "Mail Me",
-              textScaleFactor: 1.5,
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
